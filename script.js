e5e1aba25f19368e81af13d9201103fe678cc766
Tennessine23 <lmknganga@gmail.com>
2026-07-03 16:57:24 +0300

    Initialize Supabase client in script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 
+const supabaseUrl = "https://YOUR_PROJECT.supabase.co";
+const supabaseKey = "YOUR_ANON_PUBLIC_KEY";
+
+const supabase = window.supabase.createClient(
+    supabaseUrl,
+    supabaseKey
+);
